d980d2d6ae31898d10568ba1bb14d034e0ca20aa
PyLukasz <as.lukasz@gmail.com>
2023-03-08 18:11:59 +0100

    Added axios, reorganize folders, removed unused code from app.tsx

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
