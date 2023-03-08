0ee1be912949e7995f303430eb48d5383c0fc504
PyLukasz <as.lukasz@gmail.com>
2023-03-08 19:07:55 +0100

    added set startdate and enddate

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,12 +2,16 @@ import React, { FormEvent, useState, useEffect } from 'react';
 import axios from 'axios';
 import { Location } from '../models/location';
 import { Car } from '../models/car';
+import DatePicker from 'react-datepicker';
 import './App.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function App() {
   const [locations, setLocations] = useState<Location[]>([]);
   const [choosenLocation, setChoosenLocation] = useState<Location | undefined>();
   const [chosenCar, setChosenCar] = useState<Car | undefined>();
+  const [startDate, setStartDate] = useState<Date>(new Date());
+  const [endDate, setEndDate] = useState<Date>(new Date());
 
   useEffect(() => {
     axios.get<Location[]>('http://localhost:5000/api/locations')
@@ -15,7 +19,7 @@ function App() {
         setLocations(response.data)
       })
   }, [])
-  
+
   function handleLocationChange(event: FormEvent<HTMLSelectElement>) {
     setChoosenLocation(locations.find(location => location.id === Number(event.currentTarget.value)));
     setChosenCar(undefined);
@@ -30,7 +34,7 @@ function App() {
     if (!choosenLocation || !chosenCar) {
       return;
     }
-    const url = `http://localhost:5000/api/rentals/rentalCar?locationId=${choosenLocation.id}&carId=${chosenCar.id}`;
+    const url = `http://localhost:5000/api/rentals/rentalCar?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&locationId=${choosenLocation.id}&carId=${chosenCar.id}`;
     axios.post(url)
       .then(response => {
         console.log(response.data);
@@ -39,11 +43,10 @@ function App() {
         console.log(error);
       });
   }
-  
+
   return (
     <div>
-      <header>
-      <form onSubmit={handleSubmitRental} >
+        <form onSubmit={handleSubmitRental} >
           <div>
             <select onChange={handleLocationChange} className='narrow-dropdown'>
               <option value="">Select location</option>
@@ -67,10 +70,17 @@ function App() {
             </div>
           )}
           <div>
+            <label>Start date:</label>
+            <DatePicker selected={startDate} onChange={(date: Date) => setStartDate(date)} />
+          </div>
+          <div>
+            <label>End date:</label>
+            <DatePicker selected={endDate} onChange={(date: Date) => setEndDate(date)} />
+          </div>
+          <div>
             <button type="submit">Rent car</button>
           </div>
         </form>
-      </header>
     </div>
   );
 }
